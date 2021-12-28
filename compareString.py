8bed499d15a786841c9ff3aeafd56cf0af392bde
Yettsy Jo Knapp <56506858+yettsyjk@users.noreply.github.com>
2021-12-28 16:39:21 -0700

    Create compareString.py

---
+++ compareString.py
@@ -0,0 +1,26 @@
+import math
+import os
+import random
+import re
+import sys
+
+#complete twoStrings function
+#the expected function to return a STRING
+# parameters STRING s1 and STRING s2
+
+def twoStrings(sq, s2):
+    #Returns either YES or NO
+    # constraints s1 and s2 consist of characters in the range of ASCII
+    # constraints 1 <= p <= 10
+    # constraints 1 <= |s1|, |s2| <= 10^5
+    return 'YES' if set(s1) & set(s2) else 'NO'
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    q = int(input().strip())
+
+for q_itr in range(q):
+    s1 = input()
+    s2 = input()
+    result = twoStrings(s1, s2)
+    fptr.write(result + '\n')
+fptr.close()
